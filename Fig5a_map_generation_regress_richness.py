ffad952d7714855d8426c63aead4bbc36e854812
Timmywang1 <67410241+Timmywang1@users.noreply.github.com>
2023-06-10 12:33:21 +0800

    Update Fig5a_map_generation_regress_richness.py

--- Fig5a_map_generation_regress_richness.py
+++ Fig5a_map_generation_regress_richness.py
@@ -5,40 +5,57 @@ import sys
 import pandas as pd
 
 
-data = np.genfromtxt('regress_richness1.csv', delimiter = ',', filling_values=np.nan)
+data = np.genfromtxt('AP_regress_richness2023.csv', delimiter = ',', filling_values=np.nan)
 
 
-file1=gdal.Open('New_wc2.1_30s_bio_1.tif')
+file1=gdal.Open('mat.tif')
 band1=file1.GetRasterBand(1)
 mat=band1.ReadAsArray()
-file1=gdal.Open('New_wc2.1_30s_bio_12.tif')
+file1=gdal.Open('map.tif')
 band1=file1.GetRasterBand(1)
 map=band1.ReadAsArray()
-file1=gdal.Open('New_wc2.1_30s_bio_14.tif')
+file1=gdal.Open('dp.tif')
 band1=file1.GetRasterBand(1)
 dp=band1.ReadAsArray()
-file1=gdal.Open('New_Average_phh2o2.tif')
+file1=gdal.Open('ph.tif')
 band1=file1.GetRasterBand(1)
 ph=band1.ReadAsArray()
-file1=gdal.Open('New_Average_clay2.tif')
+file1=gdal.Open('clay.tif')
 band1=file1.GetRasterBand(1)
 clay=band1.ReadAsArray()
-file1=gdal.Open('New_Average_cfvo2.tif')
+file1=gdal.Open('cfv.tif')
 band1=file1.GetRasterBand(1)
 cfv=band1.ReadAsArray()
-file1=gdal.Open('New_Average_soc2.tif')
+file1=gdal.Open('soc.tif')
 band1=file1.GetRasterBand(1)
 soc=band1.ReadAsArray()
-file1=gdal.Open('New_bio21.tif')
+file1=gdal.Open('rad.tif')
 band1=file1.GetRasterBand(1)
 rad=band1.ReadAsArray()
-file1=gdal.Open('New_Contrast_01_05_1km_uint32.tif')
+file1=gdal.Open('brdf.tif')
 band1=file1.GetRasterBand(1)
 brdf=band1.ReadAsArray()
-file1=gdal.Open('New_Nadir_Reflectance_1.tif')
+file1=gdal.Open('ref.tif')
 band1=file1.GetRasterBand(1)
 ref=band1.ReadAsArray()
-
+file1=gdal.Open('biomass.tif')
+band1=file1.GetRasterBand(1)
+biomass=band1.ReadAsArray()
+file1=gdal.Open('northness.tif')
+band1=file1.GetRasterBand(1)
+northness=band1.ReadAsArray()
+file1=gdal.Open('entropy.tif')
+band1=file1.GetRasterBand(1)
+entropy=band1.ReadAsArray()
+file1=gdal.Open('evaporation.tif')
+band1=file1.GetRasterBand(1)
+evaporation=band1.ReadAsArray()
+file1=gdal.Open('hdi.tif')
+band1=file1.GetRasterBand(1)
+hdi=band1.ReadAsArray()
+file1=gdal.Open('pd.tif')
+band1=file1.GetRasterBand(1)
+pd=band1.ReadAsArray()
 
 
 mat[1,1]
@@ -71,23 +88,39 @@ brdf[brdf!=0]=1
 ref[1,1]
 ref[ref==ref[1,1]]=0
 ref[ref!=0]=1
+biomass[1,1]
+biomass[biomass==biomass[1,1]]=0
+biomass[biomass!=0]=1
+northness[1,1]
+northness[northness==northness[1,1]]=0
+northness[northness!=0]=1
+entropy[1,1]
+entropy[entropy==entropy[1,1]]=0
+entropy[entropy!=0]=1
+evaporation[1,1]
+evaporation[evaporation==evaporation[1,1]]=0
+evaporation[evaporation!=0]=1
+hdi[1,1]
+hdi[hdi==hdi[1,1]]=0
+hdi[hdi!=0]=1
+pd[1,1]
+pd[pd==pd[1,1]]=0
+pd[pd!=0]=1
 
 file1=gdal.Open('New_TotalNumber_Bootstrap_CoefVar.tif')
 band1=file1.GetRasterBand(1)
 maskocean=band1.ReadAsArray()
-maskocean[np.isnan(maskocean)] = maskocean[1,1]
-maskocean[maskocean==maskocean[1,1]]=0
-maskocean[maskocean!=0]=1
+maskocean[np.invert(np.isnan(maskocean))]=1
+maskocean[np.isnan(maskocean)]=0
 
-
-mask=mat*map*dp*ph*clay*cfv*soc*rad*brdf*ref*maskocean
+mask=mat*map*dp*ph*clay*cfv*soc*rad*brdf*ref*biomass*northness*entropy*evaporation*hdi*pd*maskocean
 
 data_mask=data*mask
 
 
 
 path1='New_Average_nitrogen2.tif'
-output_path='AP_regress_richness1.tif'
+output_path='AP_regress_richness2023.tif'
 driver=gdal.GetDriverByName('GTiff')
 file1=gdal.Open(path1)
 geotrans=file1.GetGeoTransform()
